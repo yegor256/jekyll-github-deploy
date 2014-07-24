65b39d0a9e45cc887d86dc8e3b2e070226a2c8be
Yegor Bugayenko <yegor@tpc2.com>
2014-07-24 18:30:26 +0200

    #6 travis fix again

--- test.sh
+++ test.sh
@@ -9,6 +9,8 @@ git init "${TMP}"
 cd "${TMP}"
 echo "hello" > "test.html"
 git add "test.html"
+git config user.email "test@example.com"
+git config user.name "Test"
 git commit -am 'initial commit'
 cd "${CWD}"
 
